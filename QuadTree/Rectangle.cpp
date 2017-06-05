f13af6d95ee352d5d67f3054b34b6fbfd983f309
Sourabh Singh <sourabhrishi25@gmail.com>
2017-06-05 20:57:34 +0530

    Added the is inside function

--- QuadTree/Rectangle.cpp
+++ QuadTree/Rectangle.cpp
@@ -7,7 +7,10 @@ Utils::Rectangle::Rectangle()
 
 bool Utils::Rectangle::isInside(const Rectangle &rect)
 {
-	return true;
+	if (rect.x_max <= x_max && rect.y_max <= y_max && rect.x_min <= x_min && rect.y_min <= y_min)
+		return true;
+	else
+		return false;
 }
 
 Rectangle::Rectangle(float min_x, float min_y, float max_x, float max_y)
