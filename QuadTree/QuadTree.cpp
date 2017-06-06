9db3218d047f90ec84c61001b5844927795f6640
Sourabh Singh <sourabhrishi25@gmail.com>
2017-06-06 15:05:09 +0530

    Added the insert function

--- QuadTree/QuadTree.cpp
+++ QuadTree/QuadTree.cpp
@@ -1,6 +1,41 @@
 #include "QuadTree.h"
 using namespace Utils;
 
+void Utils::QuadTree::split()
+{
+	float centerx_ = (bounds.x_min + bounds.x_max) / 2;
+	float centery_ = (bounds.y_min + bounds.y_max) / 2;
+	
+	Rectangle q1(centerx_, centery_, bounds.x_max, bounds.y_max);
+	Rectangle q2(centerx_, bounds.y_min, bounds.x_max, centery_);
+	Rectangle q3(bounds.x_min, bounds.y_min, centerx_, centery_);
+	Rectangle q4(bounds.x_min, centery_, centerx_, bounds.y_max);
+	
+	nodes[0] = (new QuadTree(level + 1, q1));
+	nodes[1] = (new QuadTree(level + 1, q2));
+	nodes[2] = (new QuadTree(level + 1, q3));
+	nodes[3] = (new QuadTree(level + 1, q4));
+}
+
+
+int Utils::QuadTree::getIndex( Rectangle& rect)
+{
+	if ( rect.isInside(nodes[0]->bounds) ){
+		return 0;
+	}
+	else if ( rect.isInside(nodes[1]->bounds) ) {
+		return 1;
+	}
+	else if (rect.isInside(nodes[2]->bounds)) {
+		return 2;
+	}
+	else if (rect.isInside(nodes[3]->bounds)) {
+		return 3;
+	}
+	return -1;
+}
+
+
 Utils::QuadTree::QuadTree()
 {
 }
@@ -21,6 +56,35 @@ void Utils::QuadTree::clear()
 	}
 	objects.clear();
 	std::cout << "Clearing level - " << level << std::endl;
+}
+
+void Utils::QuadTree::insert(Rectangle *obj)
+{
+	if (nodes[0] != NULL) {
+		int inx = getIndex(*obj);
+		if (inx != -1) {
+			nodes[inx]->insert(obj);
+			return;
+		}
+	}
+	objects.push_back(obj);
+	
+	if (objects.size() > max_objects && nodes[0] == NULL) {
+		split();
+		int end = objects.size() - 1, ite = 0;
+		while (ite != end) {
+			int inx = getIndex(*objects[ite]);
+			if (inx != -1) {
+				nodes[inx]->insert(objects[ite]);
+				Rectangle * temp = objects[ite]; objects[ite] = objects[end]; objects[end] = temp;
+				--end;
+			}
+			else
+				++ite;
+		}
+		while (objects.size() != end + 1)
+			objects.pop_back();
+	}
 }
 
 void QuadTree::setMaxObjects(int obj)
--- QuadTree/QuadTree.h
+++ QuadTree/QuadTree.h
@@ -8,16 +8,22 @@ namespace Utils {
 	class QuadTree
 	{
 	private:
+		// Variables
 		std::vector<Rectangle*> objects;
 		int max_objects = 10;
 		int max_levels = 10;
 		int level;
-		Utils::Rectangle bounds;
 		QuadTree* nodes[4];
+		// Function
+		void split();
+		int  getIndex(Rectangle&);
 	public:
+		Utils::Rectangle bounds;
 		QuadTree();
 		QuadTree(int lvl, Utils::Rectangle pbounds);
 		void clear();
+		void insert(Rectangle *);
+		void retrieve();
 		void setMaxObjects(int obj);
 		void setMaxLevels(int lvl);
 		~QuadTree();
